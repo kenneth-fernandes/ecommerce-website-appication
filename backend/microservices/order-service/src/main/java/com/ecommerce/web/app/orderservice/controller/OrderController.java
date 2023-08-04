eb254efa6457dcf4cbc2e457daa909c74ce4ab1f
Kenneth Fernandes <kenneth.peter.fernandes@gmail.com>
2023-08-04 03:40:37 -0400

    Kenneth: [Backend] - Added 2nd microservice (Order Service)

---
+++ backend/microservices/order-service/src/main/java/com/ecommerce/web/app/orderservice/controller/OrderController.java
@@ -0,0 +1,21 @@
+package com.ecommerce.web.app.orderservice.controller;
+
+import com.ecommerce.web.app.orderservice.dto.OrderRequest;
+import com.ecommerce.web.app.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order placed successfully";
+    }
+}
